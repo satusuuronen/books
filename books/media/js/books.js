13af33c0be9f4efe6028d767994f95600a02da12
Satu Suuronen <satu.t.suuronen@gmail.com>
2013-02-06 22:24:44 +0200

    List selector added

--- books/media/js/books.js
+++ books/media/js/books.js
@@ -7,3 +7,32 @@ function showInformation(button) {
         information.hide();
     }    
 }
+
+function changeList(listSelector) {
+    if ($(listSelector).hasClass('selected_list')) {
+        return;
+    }   
+    $(listSelector).parent().find('.list_title').removeClass('selected_list');
+    $(listSelector).addClass('selected_list');
+    switch(listSelector.id) {
+        case 'list1':
+            showList('listing1');
+            break;
+        case 'list2':
+            showList('listing2');
+            break;
+        case 'list3':
+            showList('listing3');
+            break;
+    }
+                
+} 
+
+function showList(list) {
+    $("#list_container").find('.list').hide();
+    $("#" + list).show();
+}
+
+function showDefaultList() {
+    showList('listing1');
+}
